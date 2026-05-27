febc9732c84a70ed708ec2a24313d0d5b43ef5ff
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-05-27 17:36:13 +0530

    Dictionary

--- basics.py
+++ basics.py
@@ -824,5 +824,43 @@ print(type(t1),type(t2),type(t3))
 
 #Dictionary
 
+dictionary = {
+"cat":"chat",
+"dog":"chien",
+"horse":"cheval"        
+}
+
+phone_numbers={'boss':5551234567, 'Suzy':5559876543}
+empty_dictionary={}
+
+print(dictionary)
+print(type(dictionary))
+print(phone_numbers)
+print(type(phone_numbers))
+print(empty_dictionary)
+print(type(empty_dictionary))
+
+
+print(dictionary["cat"])
+print(phone_numbers['boss'])
+
+#print(phone_numbers['president'])#this will give error because president is not a key in the dictionary phone_numbers.KeyError: 'president'
+
+
+words=['cat','lion','horse']
+for word in words:
+    if word in dictionary:
+        print(word, "->", dictionary[word])
+    else:
+        print(word,"is not in dictionary")
+
+
+print(dictionary.keys())
+
+for key in dictionary.keys():
+    print(key,"->", dictionary[key])
+
+
+
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,34 @@
+dictionary = {
+"cat":"chat",
+"dog":"chien",
+"horse":"cheval"        
+}
 
-var=123
-t1=(1, )
-t2=(2, )
-t3=(3,var)
+phone_numbers={'boss':5551234567, 'Suzy':5559876543}
+empty_dictionary={}
 
-t1,t2,t3=t2,t3,t1
-print(t1)
-print(t2)
-print(t3)
-print(type(t1),type(t2),type(t3))
+print(dictionary)
+print(type(dictionary))
+print(phone_numbers)
+print(type(phone_numbers))
+print(empty_dictionary)
+print(type(empty_dictionary))
+
+
+print(dictionary["cat"])
+print(phone_numbers['boss'])
+
+#print(phone_numbers['president'])#this will give error because president is not a key in the dictionary phone_numbers.KeyError: 'president'
+
+
+words=['cat','lion','horse']
+for word in words:
+    if word in dictionary:
+        print(word, "->", dictionary[word])
+    else:
+        print(word,"is not in dictionary")
+
+print(dictionary.keys())
+
+for key in dictionary.keys():
+    print(key,"->", dictionary[key])
