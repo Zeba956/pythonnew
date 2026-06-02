5178e401a34aa254c40f14b64160de432df7092c
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-06-02 18:08:16 +0530

    Multi Level Inheritance

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,31 @@
-
-class Super:
+class Level1:
+    variable_1 = 100
     def __init__(self):
-        self.supVar = 11
+        self.var_1 = 101
+    def fun_1(self):
+        return 102
 
-class Sub(Super):
+class Level2(Level1):
+    variable_2 = 200
     def __init__(self):
-        #super().__init__()#to  create inherited instance variable!
-        self.subVar = 12
+        super().__init__()
+        self.var_2 = 201
+    def fun_2(self):
+        return 202
+    
+class Level3(Level2):
+    variable_3 = 300
+    def __init__(self):
+        super().__init__()
+        self.var_3 = 301
+    def fun_3(self):
+        return 302
+    
+obj = Level3()
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
 
-obj = Sub()
-print(obj.subVar)
-print(obj.supVar)
+
+
+
