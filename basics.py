a9a3f3884e65dc7e264c767148a444beb10916fc
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-06-08 17:06:22 +0530

    exception handling

--- basics.py
+++ basics.py
@@ -1159,6 +1159,30 @@ print(example_object_3.__dict__, example_object_3.counter)
 
 
 
+class ExampleClass:
+    counter = 0
+    def __init__(self,val = 1):
+        ExampleClass.counter += 1
+        if val % 2 != 0:
+            self.a = val
+        else:
+            self.b = val
+
+example_object = ExampleClass(1)
+print(example_object.a)
+# print(example_object.b)
+
+
+#Exception Handling
+
+try:
+    print("b = ", example_object.b)
+except AttributeError:
+    pass
+
+
+
+
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,20 @@
-#08-06-2026
 class ExampleClass:
     counter = 0
     def __init__(self,val = 1):
-        self.first = val           
         ExampleClass.counter += 1
+        if val % 2 != 0:
+            self.a = val
+        else:
+            self.b = val
 
-example_object_1 = ExampleClass()
-example_object_2 = ExampleClass(2)  
-example_object_3 = ExampleClass(4)
+example_object = ExampleClass(1)
+print(example_object.a)
+# print(example_object.b)
 
-print(example_object_1.__dict__, example_object_1.counter)
-print(example_object_2.__dict__, example_object_2.counter)
-print(example_object_3.__dict__, example_object_3.counter)
+
+#Exception Handling
+
+try:
+    print("b = ", example_object.b)
+except AttributeError:
+    pass
