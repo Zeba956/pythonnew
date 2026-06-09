e83a5160663e67f9f1698765ee3c9afb502f9b84
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-06-09 18:05:11 +0530

    Exception handling

--- basics.py
+++ basics.py
@@ -1412,6 +1412,20 @@ except Exception as e:
 
 
 
+class MyZeroDivisionError(ZeroDivisionError):
+    pass
+
+def do_the_division(mine):
+    if mine:
+        raise MyZeroDivisionError("Some worse news")
+    else:
+        raise ZeroDivisionError("Some bad news")
+    
+do_the_division(False)
+
+
+
+
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,11 @@
-try:
-    i = int("Hello!")
-except Exception as e:
-    print(e)
-    print(e.__str__())
+
+class MyZeroDivisionError(ZeroDivisionError):
+    pass
+
+def do_the_division(mine):
+    if mine:
+        raise MyZeroDivisionError("Some worse news")
+    else:
+        raise ZeroDivisionError("Some bad news")
+    
+do_the_division(False)
