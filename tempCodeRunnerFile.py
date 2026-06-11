8d42e2bffa229bdeac20263422df42253c6c16e6
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-06-11 17:56:30 +0530

    Numpy

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,26 +1,8 @@
+#Pandas
+import numpy as np
+arr1d = np.array([1,2,3,4,5])
+arr2d = np.array([[85,90,78],[72,88,95],[91,76,83]])
 
-
-#CSV
-import csv
-records = [
-    ['Name','Marks','City','Grade'],
-    ['Rahul',85,'Bhopal','B'],
-    ['Priya',92,'Indore','A'],
-    ['Amit',73,'Jabalpur','B']
-]
-
-with open('students.csv','w',newline='') as f:
-    csv.writer(f).writerows(records)
-
-name = input("Enter Student Name for Search: ")
-
-with open('students.csv','r') as f:
-    for row in csv.DictReader (f):
-        if row["Name"] == name:
-            print(f'Found{name}')
-            print(f'{row["Name"]}:{row["Marks"]} marks ({row["City"]})')
-            found = True
-            break
-
-if not found:
-    print("Student Not Found!")
+print(arr2d.shape)
+print(arr2d.dtype)
+print(arr2d.ndim)
