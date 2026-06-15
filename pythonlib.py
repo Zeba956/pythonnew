6b9fdf96d6df2323d4ef1683d4680c3065430565
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-06-15 16:57:01 +0530

    Pandas

--- pythonlib.py
+++ pythonlib.py
@@ -7,3 +7,44 @@ arr2d = np.array([[85,90,78],[72,88,95],[91,76,83]])
 print(arr2d.shape)
 print(arr2d.dtype)
 print(arr2d.ndim)
+
+
+
+
+
+import pandas as pd
+
+# Create DataFrame
+data = {
+    'Name': ['Rahul', 'Priya', 'Amit', 'Sneha'],
+    'Marks': [85, 92, 73, 88],
+    'City': ['Bhopal', 'Indore', 'Jabalpur', 'Bhopal']
+}
+
+df = pd.DataFrame(data)
+
+# Select columns
+print("df['Name']:\n", df['Name'])
+print("\nName and Marks columns:")
+print(df[['Name', 'Marks']])
+
+# Filter rows
+print("\nStudents with Marks >= 85:")
+print(df[df['Marks'] >= 85])
+
+print("\nStudents from Bhopal:")
+print(df[df['City'] == 'Bhopal'])
+print(df[(df['Marks'] >= 80) & (df['City'] == 'Indore')])
+
+def get_grade(x):
+    if x >= 90:
+        return 'A'
+    elif x >= 75:
+        return 'B'
+    else:
+        return 'C'
+    
+df['Grade'] = df['Marks'].apply(get_grade)
+print(df['Grade'])
+print("-----------")
+print(df)
