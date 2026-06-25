801fc3a65e7b08234f95a28a7ceb5a68fd3488b7
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-06-25 17:17:38 +0530

    XOR Problem

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,41 @@
-askmjn
+#25-06-2026
+#Neural Learning
+#Xor problem
+
+
+import numpy as np
+
+def sigmoid(x): return 1 / (1+np.exp(-x))
+def sigmoid_d(x): return x * (1-x)
+
+X = np.array ([[0,0],[0,1],[1,0],[1,1]])
+y = np.array([[0],[1],[1],[0]])
+
+np.random.seed(42)
+W1 = np.random.randn(2,4) * 0.5
+W2 = np.random.randn(4,1) * 0.5
+
+lr = 0.5
+losses = []
+
+for epoch in range(10000):
+    h = sigmoid(X @ W1)
+    o = sigmoid(h @ W2)
+
+    loss = np.mean((y-o)**2)
+    losses.append(loss)
+
+    d_o = (o-y) * sigmoid_d(o)
+    d_h = (d_o @ W2.T) * sigmoid_d(h)
+
+
+    W2 -= lr * h.T @ d_o
+    W1 -= lr * X.T @ d_h
+
+import matplotlib.pyplot as plt
+plt.plot(losses); plt.title('Loss Decreasing During Training')
+plt.xlabel('Epoch'); plt.ylabel('Loss'); plt.show()
+
+
+print('Final predictions (should be close to [0,1,1,0]):')
+print(np.round(o,2))
